8f145e8de4eba07510df0dc47f49e6b71e7cbb7f
Matheus Olivatto <mat.franco@hotmail.com>
2024-06-05 21:19:23 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,7 +28,7 @@ public class ChessMatch {
 	public ChessPiece perfomChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
-		 validadeSourcePosition(source);
+		validadeSourcePosition(source);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -43,6 +43,9 @@ public class ChessMatch {
 	private void validadeSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
 		}
 	}
 	
